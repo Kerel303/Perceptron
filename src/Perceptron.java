89c831921a9eb60d2e2cc84a8a132763cc7c28ed
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-18 22:54:29 +0100

    Started working on Perceptron Implementation

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,6 +1,15 @@
 public class Perceptron {
+    private double weights[];
+    private double activationTreshhold;
+    private double alphaLearningConstant;
     //Przyjmujemy długość wektora wag, próg , próg włączenia perceptronu, oraz stałą uczenia alfa
     Perceptron(int LengthOfTheWeightVector, double activationTreshhold, double alphaLearningConstant){
-        
+        weights = new double[LengthOfTheWeightVector];
+        for(int i = 0; i < LengthOfTheWeightVector; i++){
+            // Początkowe wartości wag są losowane od (-1) do (1)
+            weights[i] = (Math.random()*2)-1;
+        }
+        this.activationTreshhold = activationTreshhold;
+        this.alphaLearningConstant = alphaLearningConstant;
     }
 }
