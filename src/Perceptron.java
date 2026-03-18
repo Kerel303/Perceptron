7b7ae98d1673dd48911ff49e06af1f809a3f3cf8
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-18 23:09:16 +0100

    classify function in Perceptron class

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,5 +1,5 @@
 public class Perceptron {
-    private double weights[];
+    private double[] weights;
     private double activationTreshhold;
     private double alphaLearningConstant;
     //Przyjmujemy długość wektora wag, próg , próg włączenia perceptronu, oraz stałą uczenia alfa
@@ -11,5 +11,27 @@ public class Perceptron {
         }
         this.activationTreshhold = activationTreshhold;
         this.alphaLearningConstant = alphaLearningConstant;
+    }
+
+    int classify(double[] data){
+        if(data.length != this.weights.length){
+            throw new IllegalArgumentException("Zła długość wektora wejściowego");
+        }
+
+        double sum = 0;
+        
+        for(int i = 0; i < this.weights.length; i++){
+            sum += data[i]*weights[i];
+        }
+        
+        if(sum >= this.activationTreshhold){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    void learn(double[] weights, String realAnswer){
+
     }
 }
